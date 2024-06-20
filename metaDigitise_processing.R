430efdd01d2352c82f1ae59ab47266a44b4eb664
joshuacfowler <jcf3@rice.edu>
2024-06-20 15:10:49 -0400

    adding an option to not show scientific notiation

--- metaDigitise_processing.R
+++ metaDigitise_processing.R
@@ -1,6 +1,7 @@
 
 
 library(metaDigitise)
+options(scipen = 100)# setting the options to not show scientific notation
 
 dir <- paste0(getwd(),"/paper_digitization") 
 
@@ -9,3 +10,11 @@ data <- metaDigitise(dir = dir)
 
 
 View(data)
+
+
+
+# calculating some effects sizes from Chi-square data
+library(esc)
+
+esc_chisq(22.67, totaln = 600, es.type = "d")
+esc_f()
