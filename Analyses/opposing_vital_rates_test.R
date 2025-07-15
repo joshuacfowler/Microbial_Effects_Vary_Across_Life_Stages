8ff090e7122d19421bc54a9a609a77aede4e3472
joshuacfowler <jcf3@rice.edu>
2025-07-15 15:30:42 -0600

    adding permutation test for each family

--- Analyses/opposing_vital_rates_test.R
+++ Analyses/opposing_vital_rates_test.R
@@ -30,7 +30,7 @@ multiple_metrics_df <- effects_df %>%
   mutate(positive = case_when(RII>0 ~ "positive", 
                               RII <0 ~ "negative")) %>% 
   mutate(metricXstage = paste0(metric_description, lifestage_description, sep = "_")) %>% 
-  group_by(host_species_clean, symbiont_species) %>% 
+  group_by(host_family, host_species_clean,  symbiont_species) %>% 
   dplyr::summarise(n_metrics = length(unique(metric_description)),
                    n_metricsXstage = length(unique(metricXstage)),
                    n_rows = n(),
@@ -62,12 +62,12 @@ permute_df <- effects_df %>%
                               RII <0 ~ "negative")) %>% 
   mutate(symbiota_id = paste0(host_species_clean, symbiont_species, sep = "_")) %>% 
   filter(symbiota_id %in% unique(multiple_metrics_df$symbiota_id)) %>%  
-  dplyr::select(host_species_clean, symbiont_species,metricXstage, positive)
+  dplyr::select(host_family, host_species_clean, symbiont_species,metricXstage, positive)
   
 permuted <- as_tibble(cbind(permute_df, replicate(500, sample(permute_df$positive)))) %>% 
   rename(real=positive) %>% 
   pivot_longer(cols = real:`500`, names_to = "permutation", values_to = "effect")  %>%  
-  group_by(host_species_clean, symbiont_species, permutation) %>% 
+  group_by(host_family, host_species_clean, symbiont_species, permutation) %>% 
   summarize(n_metricsXstage = length(unique(metricXstage)),
             n_rows = n(),
             n_pos = sum(effect == "positive"),
@@ -117,7 +117,7 @@ permuted_quant <- permuted_long %>%
 
 
 permutation_plot <- ggplot(permuted_counts)+
-  geom_jitter(aes(y = factor(name, levels = trt_order), x = value, color= real_or, fill = real_or, alpha = real_or, shape = real_or, size = real_or), height = .3)+
+  geom_jitter(aes(y = factor(name, levels = trt_order), x = value, color= real_or, fill = real_or, alpha = real_or, shape = real_or, size = real_or), height = .3, width = 0)+
   geom_linerange(data = permuted_quant, aes( y = factor(name, levels = trt_order), xmin = Q02.5, xmax = Q97.5), lwd = .3)+
   geom_point(data = permuted_quant, aes(y = factor(name, levels = trt_order), x = mean), shape = 18)+
   geom_point(data = real_counts, aes(y = factor(name, levels = trt_order), x = value, color= real_or, fill = real_or, alpha = real_or, shape = real_or, size = real_or))+
@@ -130,7 +130,7 @@ permutation_plot <- ggplot(permuted_counts)+
   theme_bw()
 permutation_plot
 
-ggsave(permutation_plot, filename = "Plots/permutation_plot.png", height = 2, width = 6)
+ggsave(permutation_plot, filename = "Plots/permutation_plot.png", height = 3, width = 5)
 
 
 
@@ -156,3 +156,76 @@ mean(permuted_counts[permuted_counts$name == "All Negative",]$value,)
 quantile(permuted_counts[permuted_counts$name == "All Negative",]$value, .025)
 quantile(permuted_counts[permuted_counts$name == "All Negative",]$value, .975)
 real_counts[real_counts$name == "All Negative",]
+
+
+
+
+# repeating the permutation, but analyziing by family
+family_permute_df <-  permute_df %>% 
+  mutate(symbiota = paste(host_species_clean, symbiont_species, sep = "_")) %>% 
+  group_by(host_family) %>% 
+  filter(length(unique(symbiota))>1)
+  
+
+permuted_family <- as_tibble(cbind(family_permute_df, replicate(500, sample(family_permute_df$positive)))) %>% 
+  rename(real=positive)  %>% 
+  pivot_longer(cols = real:`...506`, names_to = "permutation", values_to = "effect")  %>%  
+  group_by(host_family, host_species_clean, symbiont_species, permutation) %>% 
+  summarize(n_metricsXstage = length(unique(metricXstage)),
+            n_rows = n(),
+            n_pos = sum(effect == "positive"),
+            n_negative = sum(effect == "negative"),
+            prop_pos = n_pos/n_rows,
+            opposing = case_when(prop_pos == 1 ~ "all positive",
+                                 prop_pos == 0 ~ "all negative",
+                                 prop_pos >0 & prop_pos <1 ~ "opposing")) %>%  
+  filter(n_metricsXstage >1) %>% 
+  ungroup() %>% group_by(host_family, permutation) %>% 
+  summarize(`All Positive` =  sum(opposing == "all positive"),
+            `All Negative` = sum(opposing == "all negative"),
+            `Opposing` = sum(opposing == "opposing"))
+
+
+
+permuted_family_long <- permuted_family %>% 
+  # filter(permutation != "real") %>% 
+  pivot_longer(cols = c(`All Positive`, `All Negative`, `Opposing`)) %>% 
+  mutate(real_or = case_when(permutation == "real" ~ "Obs. data",
+                             permutation != "real" ~ "Permuted data"))
+
+permuted_family_counts <- permuted_family_long %>% filter(permutation != "real")
+real_family_counts <- permuted_family_long %>% filter(permutation == "real")
+
+
+
+trt_order <- c("All Negative", "Opposing", "All Positive")
+
+
+# calculating quantiles from permuted data
+
+permuted_family_quant <- permuted_family_long %>% 
+  filter(real_or == "Permuted data") %>% 
+  group_by(host_family, name, real_or) %>% 
+  summarize(mean = mean(value),
+            Q02.5 = quantile(value, .025),
+            Q97.5 = quantile(value, .975),
+            n = n())
+
+permutation_family_plot <- ggplot(permuted_family_counts)+
+  geom_jitter(aes(y = factor(name, levels = trt_order), x = value, color= real_or, fill = real_or, alpha = real_or, shape = real_or, size = real_or), height = .3, width = 0.05)+
+  geom_linerange(data = permuted_family_quant, aes( y = factor(name, levels = trt_order), xmin = Q02.5, xmax = Q97.5), lwd = .3)+
+  geom_point(data = permuted_family_quant, aes(y = factor(name, levels = trt_order), x = mean), shape = 18)+
+  geom_point(data = real_family_counts, aes(y = factor(name, levels = trt_order), x = value, color= real_or, fill = real_or, alpha = real_or, shape = real_or, size = real_or))+
+  scale_color_manual(values = c("red", "black"))+
+  scale_fill_manual(values = c("red", NA))+
+  scale_alpha_manual(values = c(1,.1))+
+  scale_shape_manual(values = c(16,21))+
+  scale_size_manual(values = c(2,1))+
+  facet_wrap(~host_family, scales = "free", nrow = 3)+
+  # scale_x_continuous(breaks = function(x) seq(ceiling(x[1]), floor(x[2]), by = 1))+
+  labs(x = "# of symbiota", y = "", color = "", fill = "", alpha = "", shape = "", size = "")+
+  theme_bw()
+# permutation_family_plot
+
+ggsave(permutation_family_plot, filename = "Plots/permutation_family_plot.png", height = 6, width = 10)
+
